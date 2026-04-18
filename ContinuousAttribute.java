408fcf3062125b9e13b048448cbefb99192cd1f7
fracavaliere0 <cavaliere.francesco2005@gmail.com>
2026-04-18 19:01:45 +0200

    Completata classe ContinuousAttribute con Javadoc

---
+++ ContinuousAttribute.java
@@ -0,0 +1,15 @@
+/**
+ * La classe ContinuousAttribute estende Attribute e rappresenta un attributo continuo.
+ * Modella un attributo numerico passando nome e indice alla superclasse.
+ */
+public class ContinuousAttribute extends Attribute {
+
+    /**
+     * Costruttore: invoca il costruttore della super-classe.
+     * @param name Nome simbolico dell'attributo.
+     * @param index Identificativo numerico dell'attributo.
+     */
+    public ContinuousAttribute(String name, int index) {
+        super(name, index);
+    }
+}
