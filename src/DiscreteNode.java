7330677ea9100302bd01890da243dc3f55a66155
Nickcld <nicolacialdella25@gmail.com>
2026-04-30 15:11:43 +0200

    Completata logica delle classi DiscreteNode e RegressionTree

--- src/DiscreteNode.java
+++ src/DiscreteNode.java
@@ -15,9 +15,38 @@ void setSplitInfo(Data trainingSet,int beginExampleIndex, int endExampleIndex, A
         if (!tempVal.equals(nextVal)) {
             valoriDistinti++;
         }
-        
-
     }
+        
+    mapSplit = new SplitInfo[valoriDistinti];
+    int splitIndex = 0;
+    int currentBegin = beginExampleIndex;
+
+    for (int i = beginExampleIndex; i < endExampleIndex; i++) {
+        Object tempVal = trainingSet.getExplanatoryValue(i, attribute.getIndex());
+        Object nextVal = trainingSet.getExplanatoryValue(i+1, attribute.getIndex());
+        if (!tempVal.equals(nextVal)) {
+            mapSplit[splitIndex] = new SplitInfo(tempVal, currentBegin, i, 0);
+
+            splitIndex++;
+            currentBegin = i + 1;
+        }
+    }
+        
+    // Estraggo il valore dell'ultimo blocco (basta leggere l'ultimo elemento in assoluto)
+    Object lastVal = trainingSet.getExplanatoryValue(endExampleIndex, attribute.getIndex());
+
+    // Creo lo SplitInfo finale e lo salvo
+    mapSplit[splitIndex] = new SplitInfo(lastVal, currentBegin, endExampleIndex, 0);
+}
+
+public int testCondition(Object value) {
+    for (int i = 0; i < mapSplit.length; i++) {
+        Object tempVal = mapSplit[i].getSplitValue();
+        if (tempVal.equals(value)) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 
--- src/RegressionTree.java
+++ src/RegressionTree.java
@@ -1,53 +1,90 @@
 class RegressionTree {
-		
-		RegressionTree(Data trainingSet){
-			
-			learnTree(trainingSet,0,trainingSet.getNumberOfExamples()-1,trainingSet.getNumberOfExamples()*10/100);
+
+	private Node root;
+	private RegressionTree[] childTree;
+
+	public RegressionTree() {
+
+	}
+	
+	RegressionTree(Data trainingSet){
+		learnTree(trainingSet,0,trainingSet.getNumberOfExamples()-1,trainingSet.getNumberOfExamples()*10/100);
+	}
+
+	boolean isLeaf(Data trainingSet, int begin, int end, int numberOfExamplesPerLeaf) {
+		int numberOfExamples = end - begin + 1;
+		if (numberOfExamples <= numberOfExamplesPerLeaf) {
+			return true;
 		}
-		
-		void learnTree(Data trainingSet,int begin, int end,int numberOfExamplesPerLeaf){
-			if( isLeaf(trainingSet, begin, end, numberOfExamplesPerLeaf)){
-				//determina la classe che compare pi� frequentemente nella partizione corrente
-				root=new LeafNode(trainingSet,begin,end);
-			}
-			else //split node
-			{
-				root=determineBestSplitNode(trainingSet, begin, end);
-			
-				if(root.getNumberOfChildren()>1){
-					childTree=new RegressionTree[root.getNumberOfChildren()];
-					for(int i=0;i<root.getNumberOfChildren();i++){
-						childTree[i]=new RegressionTree();
-						childTree[i].learnTree(trainingSet, ((SplitNode)root).getSplitInfo(i).beginIndex, ((SplitNode)root).getSplitInfo(i).endIndex, numberOfExamplesPerLeaf);
-					}
-				}
-				else
-					root=new LeafNode(trainingSet,begin,end);
-				
-			}
-		}
-			
+		return false;
+	}
+
+	SplitNode determineBestSplitNode(Data trainingSet, int begin, int end) {
+    	SplitNode bestNode = null;
+    	double minVariance = Double.MAX_VALUE;
+
+    	for (int i = 0; i < trainingSet.getNumberOfExplanatoryAttributes(); i++) {
+        	DiscreteAttribute currentAttribute = (DiscreteAttribute) trainingSet.getExplanatoryAttribute(i);
+        	DiscreteNode tempNode = new DiscreteNode(trainingSet, begin, end, currentAttribute);
+        
+        	double currentVariance = tempNode.getVariance();
+        
+        	// Se è la varianza più bassa trovata finora (o se è il primo giro), assegno proprio tempNode a bestNode
+        	if (bestNode == null || currentVariance < minVariance) {
+            	minVariance = currentVariance;
+            	bestNode = tempNode;
+        	}
+    	}
+
+    	// Una volta trovato il bestNode, ordino i dati del dataset in base a quell'attributo
+    	if (bestNode != null) {
+        	trainingSet.sort(bestNode.getAttribute(), begin, end);
+    	}
+    	return bestNode;
+	}
 
 		
-		void printTree(){
-			System.out.println("********* TREE **********\n");
-			System.out.println(toString());
-			System.out.println("*************************\n");
+	void learnTree(Data trainingSet,int begin, int end,int numberOfExamplesPerLeaf){
+		if (isLeaf(trainingSet, begin, end, numberOfExamplesPerLeaf)){
+			//determina la classe che compare più frequentemente nella partizione corrente
+			root=new LeafNode(trainingSet,begin,end);
 		}
-		
-		public String toString(){
-			String tree=root.toString()+"\n";
+		else //split node
+		{
+			root=determineBestSplitNode(trainingSet, begin, end);
 			
-			if( root instanceof LeafNode){
-			
+			if (root.getNumberOfChildren()>1){
+				childTree=new RegressionTree[root.getNumberOfChildren()];
+				for(int i=0;i<root.getNumberOfChildren();i++){
+					childTree[i]=new RegressionTree();
+					childTree[i].learnTree(trainingSet, ((SplitNode)root).getSplitInfo(i).beginIndex, ((SplitNode)root).getSplitInfo(i).endIndex, numberOfExamplesPerLeaf);
+				}
 			}
-			else //split node
-			{
-				for(int i=0;i<childTree.length;i++)
-					tree +=childTree[i];
+			else
+				root=new LeafNode(trainingSet,begin,end);	
+		}
+	}
+			
+
+	void printTree(){
+		System.out.println("********* TREE **********\n");
+		System.out.println(toString());
+		System.out.println("*************************\n");
+	}
+		
+	public String toString(){
+		String tree=root.toString()+"\n";
+			
+		if( root instanceof LeafNode){
+			
+		}
+		else //split node
+		{
+			for(int i=0;i<childTree.length;i++)
+				tree +=childTree[i];
 			}
 			return tree;
-		}
+	}
 		
 }
 		
