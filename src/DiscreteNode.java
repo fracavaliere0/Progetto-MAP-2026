cf0be8f6a103ef50e17457ffa5695b43d2e25787
Nickcld <nicolacialdella25@gmail.com>
2026-05-02 09:03:47 +0200

    Implementate le classi DiscreteNode e RegressionTree

--- src/DiscreteNode.java
+++ src/DiscreteNode.java
@@ -1,53 +1,78 @@
 /**
- * 
+ * Modella l'entità nodo di split relativo ad un attributo indipendente discreto.
+ * Estende la superclasse astratta SplitNode ed è responsabile della creazione 
+ * dei rami (SplitInfo) basati sui valori discreti assunti dall'attributo.
  */
 public class DiscreteNode extends SplitNode {
     
-    public DiscreteNode(Data trainingSet,int beginExampleIndex, int endExampleIndex, DiscreteAttribute attribute) {
+    /**
+     * Istanzia un oggetto invocando il costruttore della superclasse con il parametro attribute.
+     *
+     * @param trainingSet       training set complessivo
+     * @param beginExampleIndex indice nell'array del training set del primo esempio
+     * @param endExampleIndex   indice nell'array del training set dell'ultimo esempio
+     * @param attribute         attributo indipendente discreto sul quale si definisce lo split
+     */
+    public DiscreteNode(Data trainingSet, int beginExampleIndex, int endExampleIndex, DiscreteAttribute attribute) {
         super(trainingSet, beginExampleIndex, endExampleIndex, attribute);
-}
-
-void setSplitInfo(Data trainingSet,int beginExampleIndex, int endExampleIndex, Attribute attribute) {
-    int valoriDistinti = 1;
-    for (int i = beginExampleIndex; i < endExampleIndex; i++) {
-        Object tempVal = trainingSet.getExplanatoryValue(i, attribute.getIndex());
-        Object nextVal = trainingSet.getExplanatoryValue(i+1, attribute.getIndex());
-        if (!tempVal.equals(nextVal)) {
-            valoriDistinti++;
-        }
     }
-        
-    mapSplit = new SplitInfo[valoriDistinti];
-    int splitIndex = 0;
-    int currentBegin = beginExampleIndex;
 
-    for (int i = beginExampleIndex; i < endExampleIndex; i++) {
-        Object tempVal = trainingSet.getExplanatoryValue(i, attribute.getIndex());
-        Object nextVal = trainingSet.getExplanatoryValue(i+1, attribute.getIndex());
-        if (!tempVal.equals(nextVal)) {
-            mapSplit[splitIndex] = new SplitInfo(tempVal, currentBegin, i, 0);
-
-            splitIndex++;
-            currentBegin = i + 1;
+    /**
+     * Istanzia oggetti SplitInfo con ciascuno dei valori discreti dell'attributo 
+     * relativamente al sotto-insieme di training corrente, popolando l'array mapSplit.
+     *
+     * @param trainingSet       training set complessivo
+     * @param beginExampleIndex indice del primo esempio del sotto-insieme di training
+     * @param endExampleIndex   indice dell'ultimo esempio del sotto-insieme di training
+     * @param attribute         attributo indipendente sul quale si definisce lo split
+     */
+    void setSplitInfo(Data trainingSet, int beginExampleIndex, int endExampleIndex, Attribute attribute) {
+        int valoriDistinti = 1;
+        for (int i = beginExampleIndex; i < endExampleIndex; i++) {
+            Object tempVal = trainingSet.getExplanatoryValue(i, attribute.getIndex());
+            Object nextVal = trainingSet.getExplanatoryValue(i+1, attribute.getIndex());
+            if (!tempVal.equals(nextVal)) {
+                valoriDistinti++;
+            }
         }
-    }
-        
-    // Estraggo il valore dell'ultimo blocco (basta leggere l'ultimo elemento in assoluto)
-    Object lastVal = trainingSet.getExplanatoryValue(endExampleIndex, attribute.getIndex());
+            
+        mapSplit = new SplitInfo[valoriDistinti];
+        int splitIndex = 0;
+        int currentBegin = beginExampleIndex;
 
-    // Creo lo SplitInfo finale e lo salvo
-    mapSplit[splitIndex] = new SplitInfo(lastVal, currentBegin, endExampleIndex, 0);
-}
+        for (int i = beginExampleIndex; i < endExampleIndex; i++) {
+            Object tempVal = trainingSet.getExplanatoryValue(i, attribute.getIndex());
+            Object nextVal = trainingSet.getExplanatoryValue(i+1, attribute.getIndex());
+            if (!tempVal.equals(nextVal)) {
+                mapSplit[splitIndex] = new SplitInfo(tempVal, currentBegin, i, 0);
 
-public int testCondition(Object value) {
-    for (int i = 0; i < mapSplit.length; i++) {
-        Object tempVal = mapSplit[i].getSplitValue();
-        if (tempVal.equals(value)) {
-            return i;
+                splitIndex++;
+                currentBegin = i + 1;
+            }
         }
-    }
-    return -1;
-}
+            
+        // Estraggo il valore dell'ultimo blocco (basta leggere l'ultimo elemento in assoluto)
+        Object lastVal = trainingSet.getExplanatoryValue(endExampleIndex, attribute.getIndex());
 
+        // Creo lo SplitInfo finale e lo salvo
+        mapSplit[splitIndex] = new SplitInfo(lastVal, currentBegin, endExampleIndex, 0);
+    }
+
+    /**
+     * Effettua il confronto del valore in input rispetto al valore contenuto 
+     * nell'attributo splitValue di ciascuno degli oggetti SplitInfo in mapSplit.
+     *
+     * @param value valore discreto dell'attributo che si vuole testare
+     * @return l'identificativo dello split (indice dell'array mapSplit) con cui il test è positivo, -1 se non trovato
+     */
+    public int testCondition(Object value) {
+        for (int i = 0; i < mapSplit.length; i++) {
+            Object tempVal = mapSplit[i].getSplitValue();
+            if (tempVal.equals(value)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
 }
--- src/RegressionTree.java
+++ src/RegressionTree.java
@@ -1,90 +1,144 @@
+/**
+ * Modella l'intero albero di decisione come insieme di sotto-alberi.
+ * Gestisce l'induzione dell'albero a partire dai dati di training e la sua struttura ricorsiva.
+ */
 class RegressionTree {
 
-	private Node root;
-	private RegressionTree[] childTree;
+    /** Radice del sotto-albero corrente */
+    private Node root;
+    
+    /** Array di sotto-alberi originanti nel nodo root: vi è un elemento per ogni figlio */
+    private RegressionTree[] childTree;
 
-	public RegressionTree() {
+    /**
+     * Istanzia un sotto-albero vuoto dell'intero albero.
+     */
+    public RegressionTree() {
 
-	}
-	
-	RegressionTree(Data trainingSet){
-		learnTree(trainingSet,0,trainingSet.getNumberOfExamples()-1,trainingSet.getNumberOfExamples()*10/100);
-	}
+    }
+    
+    /**
+     * Istanzia un sotto-albero dell'intero albero e avvia l'induzione dell'albero 
+     * dagli esempi di training in input.
+     *
+     * @param trainingSet training set complessivo
+     */
+    RegressionTree(Data trainingSet){
+        learnTree(trainingSet,0,trainingSet.getNumberOfExamples()-1,trainingSet.getNumberOfExamples()*10/100);
+    }
 
-	boolean isLeaf(Data trainingSet, int begin, int end, int numberOfExamplesPerLeaf) {
-		int numberOfExamples = end - begin + 1;
-		if (numberOfExamples <= numberOfExamplesPerLeaf) {
-			return true;
-		}
-		return false;
-	}
+    /**
+     * Verifica se il sotto-insieme corrente può essere coperto da un nodo foglia
+     * controllando che il numero di esempi sia minore o uguale a numberOfExamplesPerLeaf.
+     *
+     * @param trainingSet             training set complessivo
+     * @param begin                   indice iniziale del sotto-insieme di training
+     * @param end                     indice finale del sotto-insieme di training
+     * @param numberOfExamplesPerLeaf numero massimo di esempi che una foglia può contenere
+     * @return true se il nodo deve diventare una foglia, false altrimenti
+     */
+    boolean isLeaf(Data trainingSet, int begin, int end, int numberOfExamplesPerLeaf) {
+        int numberOfExamples = end - begin + 1;
+        if (numberOfExamples <= numberOfExamplesPerLeaf) {
+            return true;
+        }
+        return false;
+    }
 
-	SplitNode determineBestSplitNode(Data trainingSet, int begin, int end) {
-    	SplitNode bestNode = null;
-    	double minVariance = Double.MAX_VALUE;
+    /**
+     * Determina il miglior nodo di split per il sotto-insieme di training.
+     * Prova tutti gli attributi discreti, calcola la varianza minore e ordina 
+     * la porzione di dataset rispetto all'attributo selezionato.
+     *
+     * @param trainingSet training set complessivo
+     * @param begin       indice iniziale del sotto-insieme di training
+     * @param end         indice finale del sotto-insieme di training
+     * @return il miglior nodo di split per il sotto-insieme corrente
+     */
+    SplitNode determineBestSplitNode(Data trainingSet, int begin, int end) {
+        SplitNode bestNode = null;
+        double minVariance = Double.MAX_VALUE;
 
-    	for (int i = 0; i < trainingSet.getNumberOfExplanatoryAttributes(); i++) {
-        	DiscreteAttribute currentAttribute = (DiscreteAttribute) trainingSet.getExplanatoryAttribute(i);
-        	DiscreteNode tempNode = new DiscreteNode(trainingSet, begin, end, currentAttribute);
+        for (int i = 0; i < trainingSet.getNumberOfExplanatoryAttributes(); i++) {
+            DiscreteAttribute currentAttribute = (DiscreteAttribute) trainingSet.getExplanatoryAttribute(i);
+            DiscreteNode tempNode = new DiscreteNode(trainingSet, begin, end, currentAttribute);
         
-        	double currentVariance = tempNode.getVariance();
+            double currentVariance = tempNode.getVariance();
         
-        	// Se è la varianza più bassa trovata finora (o se è il primo giro), assegno proprio tempNode a bestNode
-        	if (bestNode == null || currentVariance < minVariance) {
-            	minVariance = currentVariance;
-            	bestNode = tempNode;
-        	}
-    	}
+            // Se è la varianza più bassa trovata finora (o se è il primo giro), assegno proprio tempNode a bestNode
+            if (bestNode == null || currentVariance < minVariance) {
+                minVariance = currentVariance;
+                bestNode = tempNode;
+            }
+        }
 
-    	// Una volta trovato il bestNode, ordino i dati del dataset in base a quell'attributo
-    	if (bestNode != null) {
-        	trainingSet.sort(bestNode.getAttribute(), begin, end);
-    	}
-    	return bestNode;
-	}
+        // Una volta trovato il bestNode, ordino i dati del dataset in base a quell'attributo
+        if (bestNode != null) {
+            trainingSet.sort(bestNode.getAttribute(), begin, end);
+        }
+        return bestNode;
+    }
 
-		
-	void learnTree(Data trainingSet,int begin, int end,int numberOfExamplesPerLeaf){
-		if (isLeaf(trainingSet, begin, end, numberOfExamplesPerLeaf)){
-			//determina la classe che compare più frequentemente nella partizione corrente
-			root=new LeafNode(trainingSet,begin,end);
-		}
-		else //split node
-		{
-			root=determineBestSplitNode(trainingSet, begin, end);
-			
-			if (root.getNumberOfChildren()>1){
-				childTree=new RegressionTree[root.getNumberOfChildren()];
-				for(int i=0;i<root.getNumberOfChildren();i++){
-					childTree[i]=new RegressionTree();
-					childTree[i].learnTree(trainingSet, ((SplitNode)root).getSplitInfo(i).beginIndex, ((SplitNode)root).getSplitInfo(i).endIndex, numberOfExamplesPerLeaf);
-				}
-			}
-			else
-				root=new LeafNode(trainingSet,begin,end);	
-		}
-	}
-			
+        
+    /**
+     * Genera un sotto-albero con il sotto-insieme di input istanziando un nodo 
+     * fogliare o un nodo di split. Richiama se stesso ricorsivamente sui figli.
+     *
+     * @param trainingSet             training set complessivo
+     * @param begin                   indice iniziale del sotto-insieme di training
+     * @param end                     indice finale del sotto-insieme di training
+     * @param numberOfExamplesPerLeaf soglia limite di esempi per istanziare una foglia
+     */
+    void learnTree(Data trainingSet,int begin, int end,int numberOfExamplesPerLeaf){
+        if (isLeaf(trainingSet, begin, end, numberOfExamplesPerLeaf)){
+            //determina la classe che compare più frequentemente nella partizione corrente
+            root=new LeafNode(trainingSet,begin,end);
+        }
+        else //split node
+        {
+            root=determineBestSplitNode(trainingSet, begin, end);
+            
+            if (root.getNumberOfChildren()>1){
+                childTree=new RegressionTree[root.getNumberOfChildren()];
+                for(int i=0;i<root.getNumberOfChildren();i++){
+                    childTree[i]=new RegressionTree();
+                    childTree[i].learnTree(trainingSet, ((SplitNode)root).getSplitInfo(i).beginIndex, ((SplitNode)root).getSplitInfo(i).endIndex, numberOfExamplesPerLeaf);
+                }
+            }
+            else
+                root=new LeafNode(trainingSet,begin,end);   
+        }
+    }
+            
 
-	void printTree(){
-		System.out.println("********* TREE **********\n");
-		System.out.println(toString());
-		System.out.println("*************************\n");
-	}
-		
-	public String toString(){
-		String tree=root.toString()+"\n";
-			
-		if( root instanceof LeafNode){
-			
-		}
-		else //split node
-		{
-			for(int i=0;i<childTree.length;i++)
-				tree +=childTree[i];
-			}
-			return tree;
-	}
-		
+    /**
+     * Stampa a console le informazioni dell'intero albero avvolte in un'intestazione.
+     */
+    void printTree(){
+        System.out.println("********* TREE **********\n");
+        System.out.println(toString());
+        System.out.println("*************************\n");
+    }
+        
+    /**
+     * Concatena in una stringa tutte le informazioni della radice (root) e dei 
+     * sotto-alberi figli (childTree) invocando ricorsivamente i loro metodi toString().
+     *
+     * @return stringa testuale con la struttura dell'albero
+     */
+    public String toString(){
+        String tree=root.toString()+"\n";
+            
+        if( root instanceof LeafNode){
+            
+        }
+        else //split node
+        {
+            for(int i=0;i<childTree.length;i++)
+                tree +=childTree[i];
+            }
+            return tree;
+    }
+        
 }
 		
